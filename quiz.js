043035e922c10e5153e75e3c7c7c97b1b3f256f6
Nirmal Agrawal <nprabhat97@gmail.com>
2021-05-15 19:40:58 +0530

    initial server code

---
+++ quiz.js
@@ -0,0 +1,44 @@
+const fetch = require('node-fetch');
+
+
+
+async function getToken() {
+    try {
+        // TODO: Error Handling on the JSON response
+        const response = await fetch('https://opentdb.com/api_token.php?command=request');
+        const data = await response.json();
+        return data["token"];
+    } catch (error) {
+        res.status(500).json({
+            "error": error
+        });
+    }
+}
+
+async function getCategories() {
+    try {
+        const response = await fetch('https://opentdb.com/api_category.php');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        res.status(500).json({
+            "error": error
+        });
+    }
+}
+
+async function getQuestions(amount, category, difficulty, token = null) {
+    try {
+        const url = `https://opentdb.com/api.php?amount=${amount}${category !== '' ? '&category='+category : ''}&difficulty=${difficulty}&type=multiple`;
+        console.log(url);
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        res.status(500).json({
+            "error": error
+        });
+    }
+}
+
+module.exports = { getToken, getCategories, getQuestions }
---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const express = require('express');
+const fetch = require('node-fetch');
+const socketio = require('socket.io');
+const { getToken, getCategories, getQuestions } = require('./quiz')
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.get('/test', async (req, res) => {
+    res.send(await getCategories());
+});
+
+app.get('/test2', async (req, res) => {
+    res.send(await getQuestions(5, '', 'medium'));
+});
+
+app.get('/token', async (req, res) => {
+    res.send(await getToken());
+});
+
+io.on('connection', socket => {
+    // Generate API token on joining room
+    socket.on('joinRoom', ( { roomName, username } ) => {
+        getToken().then(() => {});
+    });
+
+    // On start game, get the questions based on the settings/params
+    socket.on('startGame', ( { questionParams /* { category, difficulty } */ } ) => {
+        getQuestions( 10, questionParams["category"], questionParams["difficulty"] ).then(() => {});
+    });
+
+    // Maybe store the questions locally and emit them one at a time
+    // OR store the whole JSON from the API and send all Q&A, check back with socket only for answers
+    // Scores after every question makes it feel more like a game and less like a test.
+});
+
+server.listen(3000, () => {
+    console.log("Server running");
+});
